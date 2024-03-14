407528891dafd89ebd366fce4a86e4e11eb53af1
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-03-14 15:07:03 +0000

    2nd

--- www/barcodeScanner.js
+++
@@ -1,7 +0,0 @@
-var BarcodeScanner = {
-    scan: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "BarcodeScanner", "scan", []);
-    }
-};
-
-module.exports = BarcodeScanner;
