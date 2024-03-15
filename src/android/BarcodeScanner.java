7c3d636de090c70f41d712666c3f2f1694519da3
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-03-15 10:02:46 +0000

    Update BarcodeScanner.java

--- src/android/BarcodeScanner.java
+++ src/android/BarcodeScanner.java
@@ -1,44 +1,39 @@
-package com.example.barcodeScanner;
+package com.example.mybarcodescanner;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Intent;
-import android.util.Log;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class BarcodeScanner extends CordovaPlugin {
-
+public class MyBarcodeScanner extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("scan")) {
-            cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    IntentIntegrator integrator = new IntentIntegrator(cordova.getActivity());
-                    integrator.setPrompt("Scan a barcode");
-                    integrator.setOrientationLocked(false);
-                    integrator.setBeepEnabled(false);
-                    integrator.initiateScan();
-                }
-            });
+        if ("scanBarcode".equals(action)) {
+            this.scanBarcode(callbackContext);
             return true;
         }
         return false;
     }
 
+    private void scanBarcode(CallbackContext callbackContext) {
+        IntentIntegrator integrator = new IntentIntegrator(cordova.getActivity());
+        integrator.setOrientationLocked(false); // Allow scanning in any orientation
+        integrator.setBeepEnabled(false); // Optional beep on scan
+        integrator.setBarcodeImageEnabled(true); // Capture barcode image
+        // Start scan
+        integrator.initiateScan();
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (result != null) {
-            if (result.getContents() != null) {
-                String scannedData = result.getContents();
-                this.callbackContext.success(scannedData);
-            } else {
-                this.callbackContext.error("Scan canceled");
-            }
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String result = scanResult.getContents();
+            this.callbackContext.success(result);
+        } else {
+            super.onActivityResult(requestCode, resultCode, intent);
         }
     }
 }
